a8abcd83fd98f54acdbda658b3a4f45eac60958e
olopcodes <olopcodes@gmail.com>
2021-04-06 22:09:38 -0400

    working on the intersection api for the rest of the sections

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const header = document.querySelector(".main-header");
 const mainHero = document.querySelector(".main-hero");
-console.log(header);
+const animateSections = document.querySelectorAll(".section--hidden");
+console.log(animateSections);
+
 const options = {
   root: null,
   threshold: 0,
-  rootMargin: `-200px`,
+  rootMargin: `-250px`,
 };
 
 const mainHeroObserver = new IntersectionObserver(function (entries, observer) {
@@ -15,7 +17,7 @@ const mainHeroObserver = new IntersectionObserver(function (entries, observer) {
   // when mainhero is not intersecting the viewport via root
   // and the -200px rootmargin makes the change happen sooner
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) {
     // add the class
@@ -30,3 +32,25 @@ const mainHeroObserver = new IntersectionObserver(function (entries, observer) {
 // observe whether the mainHero is intersecting the viewport
 mainHeroObserver.observe(mainHero);
 // observer.unobserve(mainHero);
+
+const revealSections = (entries, observer) => {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+
+  observer.unobserve(entry.target);
+};
+
+const animateSectionsObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  // threshold: 0.15,
+  threshold: 0,
+  rootMargin: `100px`,
+});
+
+animateSections.forEach((section) => {
+  animateSectionsObserver.observe(section);
+
+  section.classList.add("section--hidden");
+});
